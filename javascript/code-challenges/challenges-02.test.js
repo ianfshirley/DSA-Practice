8166bbaec75070506d7a4182e646b9bd5e98ef4c
ianfshirley <ianfshirley88@gmail.com>
2023-04-14 14:25:22 -0700

    CC2 5th test passing

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -65,7 +65,11 @@ Write a function named forEachTwoToThe that produces the same output as your for
 ------------------------------------------------------------------------------------------------ */
 
 const forEachTwoToThe = (arr) => {
-  // Solution code here...
+  let newArr = [];
+  arr.forEach((num) => {
+    newArr.push(Math.pow(2,num))
+  })
+  return newArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
