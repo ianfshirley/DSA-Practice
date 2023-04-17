7df2a453f2d663b750373ab9e9b92c5934c83e9c
ianfshirley <ianfshirley88@gmail.com>
2023-04-17 14:11:06 -0700

    CC3 all tests passing, including stretch goals

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -168,7 +168,7 @@ const characters = [
 ];
 
 const getCharactersWithoutChildren = (arr) => {
-  // Solution code here...
+  return arr.filter(character => !character.hasOwnProperty('children'));
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -180,7 +180,14 @@ For example: evenOddNumericValues(['Gregor', 2, 4, 1]) returns ['even', 'even', 
 ------------------------------------------------------------------------------------------------ */
 
 const evenOddNumericValues = (arr) => {
-  // Solution code here...
+  let newArr = arr.filter(item => typeof item === 'number');
+  return newArr.map((num) => {
+    if (num % 2 === 0) {
+      return 'even';
+    } else {
+      return 'odd';
+    }
+  })
 };
 
 /* ------------------------------------------------------------------------------------------------
