063bb096b8fa06002bb29c3c21beecbc595efbe0
ianfshirley <ianfshirley88@gmail.com>
2023-05-12 23:53:16 -0700

    CC7 3rd test passing. go back for n.2

--- javascript/code-challenges/challenges-07.test.js
+++ javascript/code-challenges/challenges-07.test.js
@@ -47,7 +47,7 @@ Write a function named joinArray that takes an array and joins all of the elemen
 ------------------------------------------------------------------------------------------------ */
 
 const joinArray = (arr) => {
-  // Solution code here...
+  return arr.join(' ');
 };
 
 /* ------------------------------------------------------------------------------------------------
