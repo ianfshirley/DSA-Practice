cbc5f2497bfb2e9476a0e3f207134be51e804c8b
ianfshirley <ianfshirley88@gmail.com>
2023-05-23 15:14:04 -0700

    CC9 3rd challenge passing

--- javascript/code-challenges/challenges-09.test.js
+++ javascript/code-challenges/challenges-09.test.js
@@ -42,7 +42,7 @@ Write a function named checkValues that takes in an object and a value and retur
 ------------------------------------------------------------------------------------------------ */
 
 const checkValues = (obj, value) => {
-  // Solution code here...
+  return Object.values(obj).includes(value);
 };
 
 /* ------------------------------------------------------------------------------------------------
