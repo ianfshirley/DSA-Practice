03909dbdfcf6601e6cb4570a09d616172c40a30e
ianfshirley <ianfshirley88@gmail.com>
2023-05-25 12:04:29 -0700

    CC10 all tests passing including stretch goals

--- javascript/code-challenges/challenges-10.test.js
+++ javascript/code-challenges/challenges-10.test.js
@@ -256,7 +256,15 @@ For example, excel('1,1,1\n4,4,4\n9,9,9') returns [3, 12, 27].
 ------------------------------------------------------------------------------------------------ */
 
 const excel = (str) => {
-  // Solution code here...
+  let rows = str.split('\n');
+  let result = [];
+  rows.forEach((row) => {
+    let sum = 0;
+    let columns = row.split(',');
+    columns.forEach(column => sum += parseInt(column));
+    result.push(sum);
+  });
+  return result;
 };
 
 /* ------------------------------------------------------------------------------------------------
