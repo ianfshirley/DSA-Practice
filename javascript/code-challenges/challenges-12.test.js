6ec9af884de1511aa85264011c766c08dbb3f9a1
ianfshirley <ianfshirley88@gmail.com>
2023-05-30 11:30:25 -0700

    CC12 5th challenge passing

--- javascript/code-challenges/challenges-12.test.js
+++ javascript/code-challenges/challenges-12.test.js
@@ -68,7 +68,8 @@ Note: if you ever need to validate an email using a regex in practice, the Inter
 ------------------------------------------------------------------------------------------------ */
 
 const validateEmail = (email) => {
-  // Solution code here...
+  let pattern = /^[a-zA-Z0-9]+.?[a-zA-Z0-9]+@[a-zA-Z0-9]+(.com|.net|.org)$/;
+  return pattern.test(email);
 };
 
 /* ------------------------------------------------------------------------------------------------
