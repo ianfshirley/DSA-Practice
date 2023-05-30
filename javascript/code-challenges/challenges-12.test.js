64060212ab98ff6e0d56b9de90e4fdf87e9df1cf
ianfshirley <ianfshirley88@gmail.com>
2023-05-30 11:03:41 -0700

    CC12 2nd test passing

--- javascript/code-challenges/challenges-12.test.js
+++ javascript/code-challenges/challenges-12.test.js
@@ -21,7 +21,8 @@ If the PIN is four numerical digits long, return true. Otherwise, return false.
 ------------------------------------------------------------------------------------------------ */
 
 const validatePin = (pin) => {
-  // Solution code here...
+  let pattern = /^\d{4}$/;
+  return pattern.test(pin);
 };
 
 /* ------------------------------------------------------------------------------------------------
